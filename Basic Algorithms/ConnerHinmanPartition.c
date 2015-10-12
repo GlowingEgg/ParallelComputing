d4592e498f39be500abe58c2c312a1047730118c
Qonzor <qhinman@gmail.com>
2015-10-12 19:49:48 -0400

    Determines the indices of an array to be summed by a particular node.

---
+++ Basic Algorithms/ConnerHinmanPartition.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct
+{
+  int *p;
+  int size;
+}chunk;
+
+chunk breakIntoChunks(int row, int col, int p, int id){
+	int i;
+	int j = 0;
+	int elements = row * col;
+	int partitionSize = elements/p+1;
+	int *partition = (int *)malloc(partitionSize * sizeof(int));
+	for(i = id; i < elements; i += p){
+		partition[j] = ((i / col) * 10) + (i % col);
+		j++;
+	}
+	chunk mychunk;
+    mychunk.p = partition;
+    mychunk.size = j + 1;
+   
+    return mychunk;
+}
+
+void display(chunk *myChunk) {
+  
+  int counter;
+  
+  for(counter = 0; counter<myChunk->size - 1; counter++) {
+     printf("%d  ", myChunk->p[counter]);
+  }
+  printf("\n");
+}
+ int main(){
+ 	int row;
+ 	int col;
+ 	int p;
+ 	int id;
+ 	printf("Number of rows: \n");
+ 	scanf("%d", &row);
+ 	printf("Number of columns: \n");
+ 	scanf("%d", &col);
+ 	printf("Number of partitions \n");
+ 	scanf("%d", &p);
+ 	printf("Partition id: \n");
+ 	scanf("%d", &id);
+ 	chunk myChunk = breakIntoChunks(row, col, p, id);
+ 	display(&myChunk);
+ }
